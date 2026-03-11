6b4b7b22d55f637075f69417735d752af1f7a589
coda192 <yusuf.ufuk@gmail.com>
2026-03-12 02:37:47 +0300

    style: replace fonts with Berkshire Swash and Roboto, fix text capitalization

--- src/components/Pages/AnaSayfa.jsx
+++ src/components/Pages/AnaSayfa.jsx
@@ -10,7 +10,7 @@ export default function AnaSayfa() {
     {
       id: 1,
       subTitle: '3 kişi 2 gece',
-      title: '3 SİLAHŞÖRLER',
+      title: '3 Silahşörler',
       price: '₺12000',
       description: '',
       features: [
@@ -25,7 +25,7 @@ export default function AnaSayfa() {
     {
       id: 2,
       subTitle: '2 kişi 2 gece',
-      title: 'HANSEL VE GRETEL',
+      title: 'Hansel ve Gretel',
       price: '₺12300',
       description: '',
       features: [
@@ -41,7 +41,7 @@ export default function AnaSayfa() {
     {
       id: 3,
       subTitle: '1 kişi 2 gece',
-      title: "ALICE'İN ANAHTARI",
+      title: "Alice'in Anahtarı",
       price: '₺15750',
       description: '',
       features: [
@@ -56,7 +56,7 @@ export default function AnaSayfa() {
     {
       id: 4,
       subTitle: '3 kişi 1 gece cuma',
-      title: 'ASLAN, CADI VE DOLAP',
+      title: 'Aslan, Cadı ve Dolap',
       price: '₺6150',
       description: '',
       features: [
@@ -70,7 +70,7 @@ export default function AnaSayfa() {
     {
       id: 5,
       subTitle: '3 kişi 1 gece cumartesi',
-      title: 'CAMELOT KONSEYİ',
+      title: 'Camelot Konseyi',
       price: '₺8750',
       description: '',
       features: [
@@ -84,7 +84,7 @@ export default function AnaSayfa() {
     {
       id: 6,
       subTitle: '2 kişi 1 gece cuma',
-      title: 'KÜÇÜK PRENS VE TİLKİ',
+      title: 'Küçük Prens ve Tilki',
       price: '₺6500',
       description: '',
       features: [
@@ -98,7 +98,7 @@ export default function AnaSayfa() {
     {
       id: 7,
       subTitle: '2 kişi 1 gece cumartesi',
-      title: 'AY IŞIĞI BALOSU',
+      title: 'Ay Işığı Balosu',
       price: '₺9100',
       description: '',
       features: [
@@ -112,7 +112,7 @@ export default function AnaSayfa() {
     {
       id: 8,
       subTitle: '1 kişi 1 gece cuma',
-      title: 'RAPUNZEL’İN KULESİ',
+      title: 'Rapunzel’in Kulesi',
       price: '₺8500',
       description: '',
       features: [
@@ -126,7 +126,7 @@ export default function AnaSayfa() {
     {
       id: 9,
       subTitle: '1 kişi 1 gece cumartesi',
-      title: 'BÜYÜLÜ BALKABAĞI',
+      title: 'Büyülü Balkabağı',
       price: '₺10750',
       description: '',
       features: [
